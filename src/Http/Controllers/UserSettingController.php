5d8c576fa239905c9594a43619d4195c10514623
Arthur REGNIER <arthur.regnier@nci.fr>
2022-04-06 10:32:05 +0200

    feat: test correction routes

--- src/Http/Controllers/UserSettingController.php
+++ src/Http/Controllers/UserSettingController.php
@@ -4,6 +4,7 @@ namespace Nci\Settings\Http\Controllers;
 
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Nci\Settings\Business\SettingBusiness;
 use Nci\Settings\Business\UserSettingBusiness;
 
@@ -11,6 +12,9 @@ class UserSettingController extends Controller
 {
     public function index(int $userId)
     {
+        if (!DB::table('user')->where('id', $userId)->first()->exists()) {
+            throw new Exception('User not found.');
+        }
         try {
             return UserSettingBusiness::get($userId);
         } catch (Exception $e) {
